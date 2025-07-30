4466a03b0baee205d3fb0bc169095a6df3a7a18b
codeleaded <codeleaded@gmail.com>
2025-07-30 03:29:27 +0200

    init commit

--- src/Main.c
+++ src/Main.c
@@ -6,112 +6,215 @@
 // #include "C:\Wichtig\System\Static\Container\Vector.h"
 
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
-#include "DebugWindow.h"
-#include "/home/codeleaded/System/Static/Library/TransformedView.h"
 #include "/home/codeleaded/System/Static/Container/Vector.h"
+#include "/home/codeleaded/System/Static/Container/Tree.h"
 
 
-TransformedView tv;
-Rect rect;
-Vec2 v;
-Vec2 a;
+#define TILESIZE	64
+#define OFFSET		0.5f
 
-DebugWindow dg_w;
+typedef struct MenuOption {
+	long id;
+	char* text;
+} MenuOption;
 
-void EventHandler(void* r,EventId* e){
-    Renderable* ra = (Renderable*)r;
-    Component* c = Component_Scene_FindRR(&dg_w.cg,ra);
-	
-    if(c){
-        char* name = Component_Attr_is(c,"name") ? (char*)Component_Attr_Get(c,"name") : NULL;
-		if(name==NULL){
-			printf("[Main]: EventHandler -> name Error!\n");
-		}else if(CStr_Cmp(name,"px")){
-			Slider* s = (Slider*)r;
-			rect.p.x = s->scrolled;
-		}else if(CStr_Cmp(name,"py")){
-			Slider* s = (Slider*)r;
-			rect.p.y = s->scrolled;
-		}else if(CStr_Cmp(name,"dx")){
-			Slider* s = (Slider*)r;
-			rect.d.x = s->scrolled;
-		}else if(CStr_Cmp(name,"dy")){
-			Slider* s = (Slider*)r;
-			rect.d.y = s->scrolled;
-		}else if(CStr_Cmp(name,"vx")){
-			Slider* s = (Slider*)r;
-			v.x = s->scrolled - 0.5f;
-		}else if(CStr_Cmp(name,"vy")){
-			Slider* s = (Slider*)r;
-			v.y = s->scrolled - 0.5f;
-		}else if(CStr_Cmp(name,"ax")){
-			Slider* s = (Slider*)r;
-			a.x = s->scrolled - 0.5f;
-		}else if(CStr_Cmp(name,"ay")){
-			Slider* s = (Slider*)r;
-			a.y = s->scrolled - 0.5f;
-		}
-    }
+MenuOption MenuOption_New(long id,char* text){
+	MenuOption mo;
+	mo.id = id;
+	mo.text = CStr_Cpy(text);
+	return mo;
 }
+void MenuOption_Free(MenuOption* mo){
+	CStr_Free(&mo->text);
+}
+void MenuOption_Print(MenuOption* mo){
+	printf(">>%ld : %s\n",mo->id,mo->text);
+}
+
+
+int Menu_X = 100;
+int Menu_Y = 200;
+
+Sprite Menu_Pointer;
+Sprite Menu_Grapper;
+Sprite Menu_Fold;
+Sprite Menu_Atlas;
+
+Vector trace;
+MenuOption* selected;
+Tree menu;
+
+char Menu_enter(){
+	Branch* b = Tree_Get_Branch(&menu,trace.size,trace.Memory);
+	return b->Childs.size>0;
+}
+int Menu_Count(){
+	Branch* b = Tree_Get_Branch(&menu,trace.size - 1,trace.Memory);
+	return b->Childs.size;
+}
+int Menu_MaxLength(int Layers){
+	Branch* b = Tree_Get_Branch(&menu,Layers,trace.Memory);
+	
+	int text_max = 0;
+
+	for(int i = 0;i<b->Childs.size;i++){
+		Branch* child = *(Branch**)Vector_Get(&b->Childs,i);
+		MenuOption* mo = (MenuOption*)child->Memory;
+		
+		if(mo && mo->text){
+			int text_new = CStr_Size(mo->text);
+			if(text_new > text_max) text_max = text_new;
+		}
+	}
+	
+	return text_max;
+}
+
+void Menu_RenderLine(int Layers,int index){
+	Branch* b = Tree_Get_Branch(&menu,Layers,trace.Memory);
+	Branch* child = *(Branch**)Vector_Get(&b->Childs,index);
+	MenuOption* mo = (MenuOption*)child->Memory;
+
+	int length = Menu_MaxLength(Layers) + 1;
+	float x = Menu_X + TILESIZE * OFFSET * Layers;
+	float y = Menu_Y + index * TILESIZE + TILESIZE * OFFSET * Layers;
+
+	Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x,y,0.0f,TILESIZE,TILESIZE,TILESIZE);
+	Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x + length * TILESIZE,y,TILESIZE * 2.0f,TILESIZE,TILESIZE,TILESIZE);
+
+	if(child->Childs.size>0)
+		Sprite_RenderAlpha(WINDOW_STD_ARGS,&Menu_Fold,x,y);
+
+	for(int i = 1;i<length;i++){
+		Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x + i * TILESIZE,y,TILESIZE,TILESIZE,TILESIZE,TILESIZE);
+	}
+
+	if(mo && mo->text)
+		RenderCStr(mo->text,x + TILESIZE,y,RED);
+}
+void Menu_RenderPart(int Layers){
+	Branch* b = Tree_Get_Branch(&menu,Layers,trace.Memory);
+	
+	int length = Menu_MaxLength(Layers) + 1;
+	float x = Menu_X + TILESIZE * OFFSET * Layers;
+	float y = Menu_Y + TILESIZE * OFFSET * Layers;
+
+	Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x,y - TILESIZE,0.0f,0.0f,TILESIZE,TILESIZE);
+	Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x + length * TILESIZE,y - TILESIZE,TILESIZE * 2.0f,0.0f,TILESIZE,TILESIZE);
+	for(int i = 1;i<length;i++){
+		Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x + i * TILESIZE,y - TILESIZE,TILESIZE,0.0f,TILESIZE,TILESIZE);
+	}
+
+	for(int i = 0;i<b->Childs.size;i++){
+		Branch* child = *(Branch**)Vector_Get(&b->Childs,i);
+		//if(child && child->Memory) printf(">> %ld %s\n",((MenuOption*)child->Memory)->id,((MenuOption*)child->Memory)->text);
+		Menu_RenderLine(Layers,i);
+	}
+
+	Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x,y + TILESIZE * (b->Childs.size),0.0f,TILESIZE * 2.0f,TILESIZE,TILESIZE);
+	Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x + length * TILESIZE,y + TILESIZE * (b->Childs.size),TILESIZE * 2.0f,TILESIZE * 2.0f,TILESIZE,TILESIZE);
+	for(int i = 1;i<length;i++){
+		Sprite_RenderSubAlpha(WINDOW_STD_ARGS,&Menu_Atlas,x + i * TILESIZE,y + TILESIZE * (b->Childs.size),TILESIZE,TILESIZE * 2.0f,TILESIZE,TILESIZE);
+	}
+}
+
 
 void Setup(AlxWindow* w){
-	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
-	TransformedView_Offset(&tv,(Vec2){ 0.0f,0.0f });
+	Menu_Pointer = Sprite_Load("./assets/Pointer.png");
+	Menu_Grapper = Sprite_Load("./assets/Grapper.png");
+	Menu_Fold = Sprite_Load("./assets/Fold.png");
+	Menu_Atlas = Sprite_Load("./assets/Flowers_Atlas.png");
 
-	rect = (Rect){ 0.0f,0.0f,0.05f,0.05f };
-	v = (Vec2){ 0.0f,0.0f };
-	a = (Vec2){ 0.0f,0.0f };
+	Sprite_Resize(&Menu_Pointer,TILESIZE,TILESIZE / 2);
+	Sprite_Resize(&Menu_Grapper,TILESIZE,TILESIZE / 2);
+	Sprite_Resize(&Menu_Fold,TILESIZE,TILESIZE);
+	Sprite_Resize(&Menu_Atlas,TILESIZE * 3,TILESIZE * 3);
 
-	ResizeAlxFont(16,16);
+	AlxFont_Resize(GetAlxFont(),TILESIZE,TILESIZE);
 
-	dg_w = DebugAlxWindow_New("./code/Main.alxml",EventHandler);
-	DebugAlxWindow_Start(&dg_w);
+	trace = Vector_New(sizeof(int));
+	menu = Tree_New();
+
+	Tree_Add(&menu,0,(int[]){   },(MenuOption[]){ MenuOption_New(0,"magic") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 0 },(MenuOption[]){ MenuOption_New(1,"potions") },sizeof(MenuOption));
+	Tree_Add(&menu,2,(int[]){ 0,0 },(MenuOption[]){ MenuOption_New(2,"heal") },sizeof(MenuOption));
+	Tree_Add(&menu,2,(int[]){ 0,0 },(MenuOption[]){ MenuOption_New(3,"damage") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 0 },(MenuOption[]){ MenuOption_New(4,"iceball") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 0 },(MenuOption[]){ MenuOption_New(5,"lightning") },sizeof(MenuOption));
+
+	Tree_Add(&menu,0,(int[]){   },(MenuOption[]){ MenuOption_New(6,"arrows") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 1 },(MenuOption[]){ MenuOption_New(7,"small") },sizeof(MenuOption));
+	Tree_Add(&menu,2,(int[]){ 1,0 },(MenuOption[]){ MenuOption_New(8,"invisible") },sizeof(MenuOption));
+	Tree_Add(&menu,2,(int[]){ 1,0 },(MenuOption[]){ MenuOption_New(9,"fast") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 1 },(MenuOption[]){ MenuOption_New(10,"big") },sizeof(MenuOption));
+	Tree_Add(&menu,2,(int[]){ 1,1 },(MenuOption[]){ MenuOption_New(11,"invisible") },sizeof(MenuOption));
+	Tree_Add(&menu,2,(int[]){ 1,1 },(MenuOption[]){ MenuOption_New(12,"fast") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 1 },(MenuOption[]){ MenuOption_New(13,"lightningarrow") },sizeof(MenuOption));
+
+	Tree_Add(&menu,0,(int[]){   },(MenuOption[]){ MenuOption_New(14,"swords") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 2 },(MenuOption[]){ MenuOption_New(15,"fireswords") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 2 },(MenuOption[]){ MenuOption_New(16,"iceswords") },sizeof(MenuOption));
+	Tree_Add(&menu,1,(int[]){ 2 },(MenuOption[]){ MenuOption_New(17,"lightningswords") },sizeof(MenuOption));
+
+	Vector_Push(&trace,(int[]){ 0 });
+
+	selected = NULL;
 }
 void Update(AlxWindow* w){
-	tv.ZoomSpeed = (float)w->ElapsedTime;
-	TransformedView_HandlePanZoom(&tv,window.Strokes,(Vec2){ GetMouse().x,GetMouse().y });
-
-	v = Vec2_Add(v,Vec2_Mulf(a,w->ElapsedTime));
-	rect.p = Vec2_Add(rect.p,Vec2_Mulf(v,w->ElapsedTime));
-
-	if(rect.p.x<0.0f){
-		rect.p.x = 0.0f;
-		v.x *= -1.0f;
+	if(Stroke(ALX_KEY_ENTER).PRESSED){
+		if(trace.size > 0){
+			selected = Tree_Get(&menu,trace.size,trace.Memory);
+		}
 	}
-	if(rect.p.y<0.0f){
-		rect.p.y = 0.0f;
-		v.y *= -1.0f;
+	
+	if(Stroke(ALX_KEY_UP).PRESSED){
+		if(trace.size > 0){
+			int* last_trace = (int*)Vector_Get(&trace,trace.size - 1);
+			if(*last_trace > 0) *last_trace -= 1;
+		}
 	}
-	if(rect.p.x>1.0f - rect.d.x){
-		rect.p.x = 1.0f - rect.d.x;
-		v.x *= -1.0f;
+	if(Stroke(ALX_KEY_DOWN).PRESSED){
+		if(trace.size > 0){
+			int* last_trace = (int*)Vector_Get(&trace,trace.size - 1);
+			if(*last_trace < Menu_Count() - 1) *last_trace += 1;
+		}
 	}
-	if(rect.p.y>1.0f - rect.d.y){
-		rect.p.y = 1.0f - rect.d.y;
-		v.y *= -1.0f;
+	if(Stroke(ALX_KEY_LEFT).PRESSED){
+		if(trace.size>0) Vector_PopTop(&trace);
+	}
+	if(Stroke(ALX_KEY_RIGHT).PRESSED){
+		if(Menu_enter()) Vector_Push(&trace,(int[]){ 0 });
 	}
 
-	Clear(BLACK);
+	Clear(WHITE);
 
-	Vec2 bg_p = TransformedView_WorldScreenPos(&tv,(Vec2){ 0.0f,0.0f });
-	Vec2 bg_d = TransformedView_WorldScreenLength(&tv,(Vec2){ 1.0f,1.0f });
-	RenderRect(bg_p.x,bg_p.y,bg_d.x,bg_d.y,BLUE);
+	for(int i = 0;i<trace.size;i++){
+		int trace_in = *(int*)Vector_Get(&trace,i);
+		Menu_RenderPart(i);
+	}
 
-	Vec2 p = TransformedView_WorldScreenPos(&tv,rect.p);
-	Vec2 d = TransformedView_WorldScreenLength(&tv,rect.d);
-	RenderRect(p.x,p.y,d.x,d.y,RED);
+	if(trace.size>0)
+		Sprite_RenderAlpha(WINDOW_STD_ARGS,&Menu_Pointer,Menu_X - Menu_Pointer.w + TILESIZE * OFFSET * (trace.size - 1),Menu_Y + 0.5f * TILESIZE + (*(int*)Vector_Get(&trace,trace.size - 1)) * TILESIZE + TILESIZE * OFFSET * (trace.size - 1));
+	
+	//Sprite_RenderAlpha(WINDOW_STD_ARGS,&Menu_Grapper,TILESIZE,0.0f);
 
-
-	String str = String_Format("| Offset: %f,%f - Zoom: %f,%f |",tv.Offset.x,tv.Offset.y,tv.Scale.x,tv.Scale.y);
-	RenderCStrSize(str.Memory,str.size,0,0,WHITE);
-	String_Free(&str);
+	if(selected && selected->text)
+		RenderCStr(selected->text,0.0f,0.0f,BLUE);
 }
 void Delete(AlxWindow* w){
-    DebugAlxWindow_Free(&dg_w);
+    Sprite_Free(&Menu_Pointer);
+	Sprite_Free(&Menu_Grapper);
+	Sprite_Free(&Menu_Fold);
+	Sprite_Free(&Menu_Atlas);
+
+	Vector_Free(&trace);
+
+	Tree_ForEach(&menu,(void*)MenuOption_Free);
+	Tree_Free(&menu);
 }
 
 int main(){
-    if(Create("Debug Window",1200,1200,1,1,Setup,Update,Delete))
+    if(Create("Menu System",1200,1200,1,1,Setup,Update,Delete))
         Start();
     return 0;
 }
